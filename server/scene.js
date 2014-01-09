9c0d14b4c8d14639dde66662d8ec745d1824c778
Vasiliy Stavenko <stavenko@gmail.com>
2014-01-10 11:00:22 +1200

    +

--- server/scene.js
+++ server/scene.js
@@ -590,6 +590,7 @@ Scene.tick = function(){
 		
 		/// Здесь мы будем достигать поставленных целей до тех импульсов, и координат, который нам нужны, для этого сначала
 		//  Попробуем на кошках - угловых моментах
+		console.log("SYN", self.need_sync, (mesh.ph_targets !== undefined));
 		if(self.need_sync && (mesh.ph_targets !== undefined)){
 			// console.log(mesh.ph_targets)
 			console.log("S");
